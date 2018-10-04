6b583f19b1a6fb0084f23f77093c5ec844bd95ad
M Chadwick <dr.m.chadwick@gmail.com>
2018-10-04 16:43:52 +0100

    Query seems to work better on a single line. Moved order by to correct position.

--- query.sql
+++ query.sql
@@ -1,14 +1,1 @@
-SELECT
-matches.match_id,
-matches.radiant_win,
-matches.start_time,
-matches.game_mode,
-matches.dire_team_id,
-matches.radiant_team_id,
-matches.leagueid,
-matches.version,
-matches.picks_bans
-FROM matches
-ORDER BY matches.match_id DESC
-WHERE matches.match_id > %MINIMUM_ID% AND matches.start_time > %MINIMUM_TIME%
-LIMIT 200 OFFSET REP_OFFSET
+SELECT matches.match_id, matches.radiant_win, matches.start_time, matches.game_mode, matches.dire_team_id, matches.radiant_team_id, matches.leagueid, matches.version, matches.picks_bans FROM matches WHERE matches.match_id > %MINIMUM_ID% AND matches.start_time > %MINIMUM_TIME% AND matches.picks_bans IS NOT NULL ORDER BY matches.match_id ASC LIMIT 200 OFFSET REP_OFFSET
