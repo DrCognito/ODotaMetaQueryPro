f5ef0a405d81c421917d0c5b6d995dcdc7607b3a
M Chadwick <dr.m.chadwick@gmail.com>
2018-11-13 13:13:20 +0000

    Removed comment.

--- update.py
+++ update.py
@@ -7,5 +7,5 @@ currentTime = Patch_7_07
 # Get our session
 session = database.getSession()
 
-#database.updateReplays(Patch_7_07, session)
+database.updateReplays(Patch_7_07, session)
 database.updateAmateurHeroes()
